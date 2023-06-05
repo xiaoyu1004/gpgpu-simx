a8e918b957516acb59e1c700e8c2e46702eff966
xiaoyu <15216142871@163.com>
2023-06-05 09:14:52 +0800

    vecAdd tests kernel

--- tests/vecAdd/kernel.c
+++ tests/vecAdd/kernel.c
@@ -3,7 +3,25 @@
 #include <vx_intrinsics.h>
 #include <vx_print.h>
 
+// #define DUMP_CSR_4(d, s)              \
+//     csr_mem[d + 0] = csr_read(s + 0); \
+//     csr_mem[d + 1] = csr_read(s + 1); \
+//     csr_mem[d + 2] = csr_read(s + 2); \
+//     csr_mem[d + 3] = csr_read(s + 3);
+
+// #define DUMP_CSR_32(d, s)      \
+//     DUMP_CSR_4(d + 0, s + 0)   \
+//     DUMP_CSR_4(d + 4, s + 4)   \
+//     DUMP_CSR_4(d + 8, s + 8)   \
+//     DUMP_CSR_4(d + 12, s + 12) \
+//     DUMP_CSR_4(d + 16, s + 16) \
+//     DUMP_CSR_4(d + 20, s + 20) \
+//     DUMP_CSR_4(d + 24, s + 24) \
+//     DUMP_CSR_4(d + 28, s + 28)
+
 void main() {
+    // asm volatile(".insn s 0x6b, 1, %1, 0(%0)" ::"r"(4), "r"(STARTUP_ADDR + 16));
+
     // activate all threads
     vx_tmc(-1);
 
@@ -16,7 +34,7 @@ void main() {
     // int num_warps  = vx_num_warps();
     int num_theads = vx_num_threads();
 
-    // int core_id   = vx_core_id();
+    int core_id   = vx_core_id();
     int warp_id   = vx_warp_id();
     int thread_id = vx_thread_id();
 
@@ -24,14 +42,15 @@ void main() {
 
     dst_ptr[idx] = src_ptr[idx];
 
+    // // set warp0 to single-threaded and stop other warps
+    // vx_tmc(0 == warp_id);
+
+    // uint32_t* const csr_mem = (uint32_t*)(IO_CSR_ADDR + 64 * sizeof(uint32_t) * core_id);
+    // DUMP_CSR_32(0, CSR_MPM_BASE)
+    // DUMP_CSR_32(32, CSR_MPM_BASE_H)
+
     // wait for all warps to complete
     vx_barrier(0, 4);
-
-    // set warp0 to single-threaded and stop other warps
-    // int wid = vx_warp_id();
-    vx_tmc(0 == warp_id);
-
-    vx_perf_dump();
 
     vx_tmc(0);
 }
